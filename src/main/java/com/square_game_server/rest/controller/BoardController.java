41d2fa349419b1149cfedc636250bf5014535bb7
sascrystal <ru.da.sas@bk.ru>
2025-09-24 23:58:08 +0300

    добавлен restController для Board и добавлен get запрос board/checkWinner который проверяет победу.

---
+++ src/main/java/com/square_game_server/rest/controller/BoardController.java
@@ -0,0 +1,23 @@
+package com.square_game_server.rest.controller;
+
+
+import com.square_game_server.domain.Board;
+import com.square_game_server.rest.Dto.BoardDto;
+import com.square_game_server.service.CellService;
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@AllArgsConstructor
+public class BoardController {
+    private final CellService cellService;
+
+    @GetMapping("board/checkWinner")
+    private String checkWinner(@RequestBody BoardDto boardDto) {
+        Board board = BoardDto.boardDtoToDomainObject(boardDto);
+        return cellService.calculateWinner(board.getData());
+    }
+}
--- src/main/java/com/square_game_server/rest/controller/GameController.java
+++ src/main/java/com/square_game_server/rest/controller/GameController.java
@@ -4,14 +4,13 @@ import com.square_game_server.domain.Board;
 import com.square_game_server.domain.SimpleMove;
 import com.square_game_server.rest.Dto.BoardDto;
 import com.square_game_server.rest.Dto.SimpleMoveDto;
-import com.square_game_server.service.BotRandom;
 import com.square_game_server.service.BotService;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @AllArgsConstructor
-public class gameController {
+public class GameController {
     private final BotService botService;
     @PostMapping("/api/{rules}/nextMove")
     public SimpleMoveDto nextMove(@PathVariable("rules") String rules, @RequestBody BoardDto boardDto) {
@@ -19,4 +18,5 @@ public class gameController {
         SimpleMove simpleMove = botService.doMove(board);
         return SimpleMoveDto.domainObjectToDto(simpleMove);
     }
+
 }
