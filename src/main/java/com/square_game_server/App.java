4167e2f0f0b4d84ac8c52a59418e090b5160cfcb
sascrystal <ru.da.sas@bk.ru>
2025-09-28 10:57:41 +0300

    Настройки BotPriority перенесен в application.yml

--- src/main/java/com/square_game_server/App.java
+++ src/main/java/com/square_game_server/App.java
@@ -1,7 +1,9 @@
 package com.square_game_server;
 
+import com.square_game_server.config.BotPriorityConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
---
+++ src/main/java/com/square_game_server/config/BotPriorityConfig.java
@@ -0,0 +1,19 @@
+package com.square_game_server.config;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
+
+@Component
+@Configuration
+@ConfigurationProperties(prefix = "bot.priority")
+@Data
+public class BotPriorityConfig {
+    private   int pointsForFriendlyPiece0;
+    private   int pointsForFriendlyPiece1;
+    private   int pointsForFriendlyPiece2;
+    private   int pointsForEnemyPiece0;
+    private   int pointsForEnemyPiece1;
+    private   int pointsForEnemyPiece2;
+}
